92120f2ba04a0d1af071ddf200cef7e0ca90de91
Sievers,Paul <Paul.Sievers@wwt.com>
2018-10-21 17:27:19 -0500

    initial commit

---
+++ test_employee.py
@@ -0,0 +1,16 @@
+import unittest
+from employee import Employee
+
+
+class TestEmployee(unittest.TestCase):
+
+    def setUp(self):
+        self.emp_1 = Employee("John", "Smith", 50000)
+        self.emp_2 = Employee("Susan", "Anderson", 60000)
+
+    def tearDown(self):
+        pass
+
+    def test_email(self):
+        self.assertEqual(self.emp_1.email, 'John.Smith@company.com')
+        self.assertEqual(self.emp_2.email, 'Susan.Anderson@company.com')
